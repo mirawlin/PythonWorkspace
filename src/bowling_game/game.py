76e5fd134eda090c52bb6ca4cb4d2dc34b6e9837
miranda lin <miranda.lin@online-pajak.com>
2022-09-29 14:32:05 +0200

    implemented spare logic

--- src/bowling_game/game.py
+++ src/bowling_game/game.py
@@ -1,11 +1,21 @@
 
 
 class Game:
-    total_score = 0
+    rolls = []
 
     def score(self) -> int:
-        return self.total_score
+        score = 0
+        roll_number = 0
+        for frame in range(10):
+            if self.rolls[roll_number] + self.rolls[roll_number+1] == 10:
+                score += 10 + self.rolls[roll_number+2]
+                roll_number += 2
+            else:
+                score += self.rolls[roll_number] + self.rolls[roll_number+1]
+                roll_number += 2
+
+        return score
 
     def roll(self, pins: int):
-        self.total_score += pins
+        self.rolls.append(pins)
 
--- tests/bowling_game/test_game.py
+++ tests/bowling_game/test_game.py
@@ -21,6 +21,19 @@ def test_game_with_all_1_pins_should_have_score_20():
     assert score == 20
 
 
+def test_game_with_one_spare_should_compute_bonus_correctly():
+    game = Game()
+
+    game.roll(5)
+    game.roll(5)
+    game.roll(3)
+    play_game(game, 17, 0)
+
+    score = game.score()
+
+    assert score == 16
+
+
 def play_game(game: Game, rolls: int, pins: int):
     for i in range(0, rolls):
         game.roll(pins)
